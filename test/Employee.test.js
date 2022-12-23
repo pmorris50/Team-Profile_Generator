3c8f71ad5bae919bdd8d7fe75bc33d1fe9c166ac
pmorris50 <patrickmorris532@gmail.com>
2022-12-23 10:21:08 -0700

    testing

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,10 +3,23 @@ const berry = new Employee('berry', '20', 'b@email.com')
 
 describe('testing Employee class', ()=>{
    it('testing employee name key', ()=>{
-    expect(berry.name).toBe('berry')
+    expect(berry.name).toBe('berry');
    })
    it('testing employee name method', ()=>{
-    expect(berry.getName()).toBe('berry')
+    expect(berry.getName()).toBe('berry');
    })
+   it('testing employee id key', ()=>{
+      expect(berry.id).toBe('20')
+   })
+   it('testing employee email key', ()=>{
+      expect(berry.email).toBe('b@email.com');
+   })
+   it('testing employee id method', ()=>{
+      expect(berry.getId()).toBe('b@email.com');
+   })
+   it('testing employee role method', () => {
+      expect(berry.getRole()).toBe('Employee')
+   })
+
 
 })
